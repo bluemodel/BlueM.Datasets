42ab53b8e4ae83a6ce6999c63f3368d97de90773
jamaa <90166+jamaa@users.noreply.github.com>
2023-02-25 09:26:37 +0100

    make input paths relative, restore original inputs, delete backup files

--- SWMM/A128_OPTI/Create_A128.bat
+++
@@ -1,9 +0,0 @@
-del A128.inp
-del A128.hsf
-del A128.rpt
-del A128.out
-copy AB_A128.inp A128.inp
-copy AB_A128.mod A128.mod
-copy AB_A128.opt A128.opt
-copy AB_A128.zie A128.zie
-copy A128_2100.hsf A128.hsf
